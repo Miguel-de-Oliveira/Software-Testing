0c550cd2b1cf4aa2a3d6492aa267a4583fd4d4fd
Miguel Luiz <miguelluizldeoliveira@gmail.com>
2026-03-25 17:59:14 -0300

    Adição de uma classe calculadora.

---
+++ Software_Testing/Teste_Unitario/Teste_Unitario_Automatizado/Calculadora.java
@@ -0,0 +1,16 @@
+package Teste_Unitario_Automatizado;
+
+public class Calculadora {
+	public double soma(double n1, double n2){
+		return n1 + n2;
+	}
+	public double subtracao(double n1, double n2){
+		return n1 - n2;
+	}
+	public double multiplicacao(double n1, double n2){
+		return n1 * n2;
+	}
+	public double divisao(double n1, double n2){
+		return n1 / n2;
+	}
+}
